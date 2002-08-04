19d21d9916106facaf2cf872c099c26a0854d15c
Michiel Broek <mbse@mbse.eu>
2002-08-04 10:52:51 +0000

    Added queue sources

---
+++ mbfido/queue.c
@@ -0,0 +1,42 @@
+/*****************************************************************************
+ *
+ * $Id$
+ * Purpose ...............: Mail and file queue operations
+ *
+ *****************************************************************************
+ * Copyright (C) 1997-2002
+ *   
+ * Michiel Broek		FIDO:		2:280/2802
+ * Beekmansbos 10
+ * 1971 BV IJmuiden
+ * the Netherlands
+ *
+ * This file is part of MBSE BBS.
+ *
+ * This BBS is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2, or (at your option) any
+ * later version.
+ *
+ * MBSE BBS is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with MBSE BBS; see the file COPYING.  If not, write to the Free
+ * Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *****************************************************************************/
+
+#include "../config.h"
+#include "../lib/libs.h"
+#include "../lib/memwatch.h"
+#include "../lib/structs.h"
+#include "../lib/users.h"
+#include "../lib/records.h"
+#include "../lib/common.h"
+#include "../lib/clcomm.h"
+#include "queue.h"
+
+
+
---
+++ mbfido/queue.h
@@ -0,0 +1,6 @@
+#ifndef	_QUEUE_H
+#define	_QUEUE_H
+
+/* $Id$ */
+
+#endif
