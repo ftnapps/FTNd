4411180f3c4766238f3c6e78b5a57ab266aecc61
Michiel Broek <mbse@mbse.eu>
2004-08-08 15:22:21 +0000

    Updated bbsdoor.sh for dosemu 1.2.0 and later

--- script/bbsdoor.sh
+++ script/bbsdoor.sh
@@ -8,17 +8,28 @@
 #
 #  by Redy Rodriguez, 22-Oct-2001
 
+DOSDRIVE=/dos/c
+
 if [ "$1" != "" ]; then
     if [ "$2" != "" ]; then
-	mkdir -p /dos/c/doors/node$2 >/dev/null 2>&1
+	mkdir -p $DOSDRIVE/doors/node$2 >/dev/null 2>&1
 	# Copy door.sys to dos partition
-	cat ~/door.sys >/dos/c/doors/node$2/door.sys
-	# Create .dosemu/disclaimer in user home to avoid warning
+	cat ~/door.sys >$DOSDRIVE/doors/node$2/door.sys
+	# Create .dosemu directory for the user.
 	if [ ! -d $HOME/.dosemu ]; then
 	    mkdir $HOME/.dosemu
 	fi
+	# Looks cheap, see above, but this does an upgrade too
+	if [ ! -d $HOME/.dosemu/drives ]; then
+	    mkdir $HOME/.dosemu/drives
+	fi
+	# Create .dosemu/disclaimer in user home to avoid warning
 	if [ ! -f $HOME/.dosemu/disclaimer ]; then
 	    touch $HOME/.dosemu/disclaimer
+	fi
+	# For dosemu 1.2.0 and later, create a link to the drive
+	if [ ! -f $HOME/.dosemu/drives/c ]; then
+	    ln -s $DOSDRIVE $HOME/.dosemu/drives/c
 	fi
     fi
 fi
