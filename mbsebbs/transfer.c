2badad0ae59d13b1c3f6618870e209396775b017
Michiel Broek <mbse@mbse.eu>
2004-11-05 15:54:31 +0000

    Added basic filetranfer sources

---
+++ mbsebbs/transfer.c
@@ -0,0 +1,42 @@
+/*****************************************************************************
+ *
+ * $Id$
+ * Purpose ...............: File Transfers
+ *
+ *****************************************************************************
+ * Copyright (C) 1997-2004
+ *   
+ * Michiel Broek		FIDO:		2:280/2802
+ * Beekmansbos 10
+ * 1971 BV IJmuiden
+ * the Netherlands
+ *
+ * This file is part of MBSE BBS.
+ *
+ * This BBS is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2, or (at your option) any
+ * later version.
+ *
+ * MBSE BBS is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with MBSE BBS; see the file COPYING.  If not, write to the Free
+ * Software Foundation, 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ *****************************************************************************/
+
+#include "../config.h"
+#include "../lib/mbselib.h"
+#include "../lib/mbse.h"
+#include "../lib/users.h"
+#include "transfer.h"
+
+
+int transfer(down_list **download_list, up_list **upload_list, int direction)
+{
+    return 0;
+}
+
---
+++ mbsebbs/transfer.h
@@ -0,0 +1,37 @@
+#ifndef	_TRANSFER_H
+#define	_TRANSFER_H
+
+/* $Id$ */
+
+typedef enum	{FT_UPLOAD, FT_DOWNLOAD, FT_BIDIRECT} TRANSFERTYPE;
+
+
+/*
+ *  List of files to download.
+ */
+typedef struct _down_list {
+	struct _down_list	*next;
+	char			*local;		/* Local filename	*/
+	char			*remote;	/* Remove filename	*/
+	long			cps;		/* CPS after sent	*/
+	unsigned		kfs	: 1;	/* Kill File Sent	*/
+	unsigned		sent	: 1;	/* File is Sent		*/
+} down_list;
+
+
+/*
+ *  List of uploaded files.
+ */
+typedef struct _up_list {
+	struct _up_list		*next;
+	char			*remote;	/* Remote filename	*/
+	char			*local;		/* Local filename	*/
+	long			cps;		/* CPS after received	*/
+	unsigned		success	: 1;	/* If received Ok.	*/
+} up_list;
+
+
+int transfer(down_list **, up_list **, int);
+
+
+#endif
