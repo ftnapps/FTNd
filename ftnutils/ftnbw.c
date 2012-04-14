b2859e50f69211be2dd3d54e59ea6ec6b73e656c
Robert James Clay <jame@rocasa.us>
2012-04-14 15:18:11 -0400

    Initial adaption of ftnutils/ftnbw.c for FTNd.

--- ftnutils/ftnbw.c
+++ ftnutils/ftnbw.c
@@ -1,35 +1,31 @@
 /*****************************************************************************
  *
- * $Id: mbbw.c,v 1.4 2007/08/25 18:32:09 mbse Exp $
+ * $Id: ftnbw.c
  * Purpose ...............: Dump Bluewave packets
  *
  *****************************************************************************
- * Copyright (C) 1997-2007
- *   
- * Michiel Broek		FIDO:	2:280/2802
- * Beekmansbos 10
- * 1971 BV IJmuiden
- * the Netherlands
+ * Copyright (C)    2012   Robert James Clay <jame@rocasa.us>
+ * Copyright (C) 1997-2007 Michiel Broek <mbse@mbse.eu>
  *
- * This file is part of MBSE BBS.
+ * This file is part of FTNd.
  *
- * This BBS is free software; you can redistribute it and/or modify it
+ * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation; either version 2, or (at your option) any
  * later version.
  *
- * MBSE BBS is distributed in the hope that it will be useful, but
+ * FTNd is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with MBSE BBS; see the file COPYING.  If not, write to the Free
+ * along with FTNd; see the file COPYING.  If not, write to the Free
  * Software Foundation, 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
  *****************************************************************************/
 
 #include "../config.h"
-#include "../lib/mbselib.h"
+#include "../lib/ftnselib.h"
 #include "../lib/bluewave.h"
 
 
@@ -44,11 +40,11 @@ int main(int argc, char **argv)
     FTI_REC	    Fti;
     int		    i, pos;
 
-    printf("\nMBBW: MBSE BBS %s Bluewave Dump\n", VERSION);
+    printf("\nFTNBW: FTNd %s Bluewave Dump\n", VERSION);
     printf("      %s\n\n", COPYRIGHT);
 
     if (argc != 2) {
-	printf("Usage:    mbbw [packetname]\n\n");
+	printf("Usage:    ftnbw [packetname]\n\n");
 	return 0;
     }
 
