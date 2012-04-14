9e4044ee1ea9908e8c04775232dded13dba2652f
Robert James Clay <jame@rocasa.us>
2012-04-14 15:24:19 -0400

    Initial adaption of ftnutils/ftnlang.c for FTNd.

--- ftnutils/ftnlang.c
+++ ftnutils/ftnlang.c
@@ -1,6 +1,6 @@
 /*****************************************************************************
  *
- * $Id: mblang.c,v 1.2 2005/08/30 17:45:31 mbse Exp $
+ * ftnlang.c
  * Purpose ...............: Language Compiler
  *
  *****************************************************************************
@@ -11,25 +11,25 @@
  * 1971 BV IJmuiden
  * the Netherlands
  *
- * This file is part of MBSE BBS.
+ * This file is part of FTNd.
  *
  * This BBS is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation; either version 2, or (at your option) any
  * later version.
  *
- * MBSE BBS is distributed in the hope that it will be useful, but
+ * FTNd is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with MBSE BBS; see the file COPYING.  If not, write to the Free
+ * along with FTNd; see the file COPYING.  If not, write to the Free
  * Software Foundation, 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
  *****************************************************************************/
 
 #include "../config.h"
-#include "../lib/mbselib.h"
+#include "../lib/ftndlib.h"
 
 
 int main(int argc, char **argv)
@@ -41,12 +41,12 @@ int main(int argc, char **argv)
 	temp  = calloc(PATH_MAX, sizeof(char));
 	temp1 = calloc(PATH_MAX, sizeof(char));  
 
-	printf("\nMBLANG: MBSE BBS %s Quick Language Data File Creator\n", VERSION);
+	printf("\nFTNLANG: FTNd %s Quick Language Data File Creator\n", VERSION);
 	printf("        %s\n", COPYRIGHT);
 
 	if (argc < 3) {
 		printf("\nUsage: %s [language data file] [language text file]\n\n", *(argv));
-		exit(MBERR_COMMANDLINE);
+		exit(FTNERR_COMMANDLINE);
 	}
 
 	snprintf(temp1, PATH_MAX, "%s", *(argv + 1));
@@ -55,12 +55,12 @@ int main(int argc, char **argv)
 	snprintf(temp, PATH_MAX, "%s", *(argv + 2));
 	if ((fp1 = fopen(temp, "r")) == NULL) {
 		printf("\nUnable to open %s\n", temp);
-		exit(MBERR_COMMANDLINE);
+		exit(FTNERR_COMMANDLINE);
 	}
 	snprintf(temp1, PATH_MAX, "%s", *(argv + 1));
 	if ((fp = fopen(temp1, "a+")) == NULL) {
 		printf("\nUnable to open %s\n", temp1);
-		exit(MBERR_COMMANDLINE);
+		exit(FTNERR_COMMANDLINE);
 	}
 
 	lines = 0;
@@ -78,7 +78,7 @@ int main(int argc, char **argv)
 		}
 		if (i > 29) {
 			printf("\nKey part in line %d too long (%d chars)", lines, i);
-			exit(MBERR_GENERAL);
+			exit(FTNERR_GENERAL);
 		}
 
 		/*
@@ -93,7 +93,7 @@ int main(int argc, char **argv)
 		}
 		if (j > 84) {
 			printf("\nLanguage string in line %d too long (%d chars)", lines, j);
-			exit(MBERR_GENERAL);
+			exit(FTNERR_GENERAL);
 		}
 
 		fwrite(&ldata, sizeof(ldata), 1, fp);
