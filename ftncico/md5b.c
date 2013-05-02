23d1db55b6edf56a181f5b6365f0f946e3cf218a
Robert James Clay <jame@rocasa.us>
2013-05-01 20:07:35 -0400

    Updates to ftncico/md5b.c and ftncico/zmmisc.c for FTNd.

--- ftncico/md5b.c
+++ ftncico/md5b.c
@@ -1,30 +1,26 @@
 /*****************************************************************************
  *
- * $Id: md5b.c,v 1.5 2007/08/25 15:29:14 mbse Exp $
+ * md5b.c
  * Purpose ...............: MD5 for binkp protocol driver
  *
  *****************************************************************************
- * Copyright (C) 1997-2007
- *   
- * Michiel Broek                FIDO:   2:280/2802
- * Beekmansbos 10
- * 1971 BV IJmuiden
- * the Netherlands
+ * Copyright (C) 1997-2007 Michiel Broek <mbse@mbse.eu>
+ * Copyright (C)    2013   Robert James Clay <jame@rocasa.us>
  *
- * This file is part of MBSE BBS.
+ * This file is part of FTNd.
  *
- * This BBS is free software; you can redistribute it and/or modify it
+ * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation; either version 2, or (at your option) any
  * later version.
  *
- * MBSE BBS is distributed in the hope that it will be useful, but
+ * FTNd is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with MBSE BBS; see the file COPYING.  If not, write to the Free
+ * along with FTNd; see the file COPYING.  If not, write to the Free
  * Software Foundation, 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
  *****************************************************************************/
 
@@ -55,7 +51,7 @@ documentation and/or software.
  */
 
 #include "../config.h"
-#include "../lib/mbselib.h"
+#include "../lib/ftndlib.h"
 #include "../lib/nodelist.h"
 #include "lutil.h"
 #include "md5b.h"
--- ftncico/zmmisc.c
+++ ftncico/zmmisc.c
@@ -63,7 +63,7 @@ static int zgeth1(void);
 static void garbitch(void);
 
 #include "../config.h"
-#include "../lib/mbselib.h"
+#include "../lib/ftndlib.h"
 #include "../lib/nodelist.h"
 #include "ttyio.h"
 #include "session.h"
