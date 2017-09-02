cd7821eeb5d1d0f3a890c8f9c200cf7df3de2d56
Robert James Clay <jame@rocasa.us>
2017-09-02 09:34:21 -0400

    Correct 'Wich' as 'Which' on line 574 of ftnsetup/m_farea.c

--- ftnsetup/m_farea.c
+++ ftnsetup/m_farea.c
@@ -571,7 +571,7 @@ void EditFilearea(void)
 		if (strncmp(pick, "M", 1) == 0) {
 		    from = too = 0;
 		    ftnd_mvprintw(LINES -3, 5, "From");
-		    from = edit_int(LINES -3, 10, from, (char *)"Wich ^area^ you want to move");
+		    from = edit_int(LINES -3, 10, from, (char *)"Which ^area^ you want to move");
 		    ftnd_mvprintw(LINES -3,15, "To");
 		    too  = edit_int(LINES -3, 18, too,  (char *)"Too which ^area^ to move");
 
