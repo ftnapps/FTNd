815bf4f41eed2e561b771f1cf74cef7a8aea46c1
Robert James Clay <jame@rocasa.us>
2017-09-02 14:53:52 -0400

    Correct FSF address in ftnbbs/*.c files as necessary.

--- ftnbbs/ymsend.c
+++ ftnbbs/ymsend.c
@@ -4,24 +4,24 @@
  * Purpose ...............: Ymodem sender
  *
  *****************************************************************************
+ * Copyright (C) 2013-2017 Robert James Clay <jame@rocasa.us>
  * Copyright (C) 1997-2005 Michiel Broek <mbse@mbse.eu>
- * Copyright (C)    2013   Robert James Clay <jame@rocasa.us>
  *
  * This file is part of FTNd.
  *
- * This BBS is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2, or (at your option) any
- * later version.
+ * This is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2, or (at your option) any later
+ * version.
  *
  * FTNd is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License
- * along with FTNd; see the file COPYING.  If not, write to the Free
- * Software Foundation, 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ * You should have received a copy of the GNU General Public License along
+ * with FTNd; see the file COPYING.  If not, write to the Free Software
+ * Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *****************************************************************************/
 
 #include "../config.h"
